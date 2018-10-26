9edd50f743032d88904648fef52651b9b1d851d2
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2018-10-26 19:47:34 +0530

    added extra validation for atrule object

--- index.js
+++ index.js
@@ -21,18 +21,20 @@ export default postcss.plugin('postcss-reverse-props', (options = {
              * if decorator is '@mobile' remove only decorator (preventing scss/less to process decorators.)
              * This utility should be used before any other preprocessors.
              */
-            options.exclude.forEach(element => {
-                if (atrule.name && atrule.name === element) {
-                    atrule.remove();
-                }
-            });
-            options.include.forEach(element => {
-                if (atrule.name && atrule.name === element) {
-                    const rule = postcss.rule({ selector: atrule.params, source: atrule.source, nodes: atrule.nodes })
-                    atrule.replaceWith(rule);
-                }
-            });
-
+            if(typeof atrule === "object"){
+                options.exclude.forEach(element => {
+                    if (atrule.name && atrule.name === element) {
+                        atrule.remove();
+                    }
+                });
+                options.include.forEach(element => {
+                    //using hasOwnProperty to avoid conflicts with prototype
+                    if (atrule.hasOwnProperty("name") && atrule.name === element) {
+                        const rule = postcss.rule({ selector: atrule.params, source: atrule.source, nodes: atrule.nodes })
+                        atrule.replaceWith(rule);
+                    }
+                });
+            }
         });
     };
 });
